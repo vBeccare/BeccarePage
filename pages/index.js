c215feb80d9bde97e6d0e971f1306d47d407f44f
Victor Beccare <vpdr1912@gmail.com>
2022-01-06 10:32:06 -0300

    Feat: adding image

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
+import Image from "next/image";
+
 export default () => (
-	<main>
-		<h1>Rodrigo é complicado Next.js</h1>
-	</main>
-)
+  <main>
+    <h1>Rodrigo é complicado Next.js</h1>
+
+    <Image src="/rodrigo-romuleira.png" width="400" height="300"/>
+  </main>
+);
